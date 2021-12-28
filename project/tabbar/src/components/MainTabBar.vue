efc80262c52ee3dac2f6b665fdc17d4b35d9ed00
apple <123@qq.com>
2021-12-28 17:00:08 +0800

    路径别名alias

--- project/tabbar/src/components/MainTabBar.vue
+++ project/tabbar/src/components/MainTabBar.vue
@@ -1,18 +1,18 @@
 <template>
     <tab-bar>
       <tab-bar-item path="/home" :activeColor="activeColor">
-        <img slot="item-icon" src="../assets/img/tabbar/1-1.png">
-        <img slot="item-icon-active" src="../assets/img/tabbar/1-11.png">
+        <img slot="item-icon" src="~assets/img/tabbar/1-1.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/1-11.png">
         <span slot="item-text">首页</span>
       </tab-bar-item>
       <tab-bar-item path="/category" :activeColor="activeColor">
-        <img slot="item-icon" src="../assets/img/tabbar/2-1.png">
-        <img slot="item-icon-active" src="../assets/img/tabbar/2-11.png">
+        <img slot="item-icon" src="~assets/img/tabbar/2-1.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/2-11.png">
         <span slot="item-text">分类</span>
       </tab-bar-item>
       <tab-bar-item path="/car" :activeColor="activeColor">
-        <img slot="item-icon" src="../assets/img/tabbar/3-1.png">
-        <img slot="item-icon-active" src="../assets/img/tabbar/3-11.png">
+        <img slot="item-icon" src="~assets/img/tabbar/3-1.png">
+        <img slot="item-icon-active" src="~assets/img/tabbar/3-11.png">
         <span slot="item-text">购物车</span>
       </tab-bar-item>
       <tab-bar-item path="/profile" :activeColor="activeColor">
@@ -24,8 +24,8 @@
 </template>
 
 <script>
-import TabBarItem from './tabbar/TabBarItem.vue'
-import TabBar from './tabbar/TabBar.vue'
+import TabBarItem from 'components/tabbar/TabBarItem.vue'
+import TabBar from 'components/tabbar/TabBar.vue'
 export default {
   components: { TabBar, TabBarItem },
   name: 'MainTabBar',
