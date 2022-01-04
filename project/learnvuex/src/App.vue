bb8897aed9a6a8891dd38f3b841b723acda57cfe
apple <123@qq.com>
2022-01-04 15:12:40 +0800

    单页面状态管理

---
+++ project/learnvuex/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div>当前计数：{{counter}}</div>
+    <button @click="counter++">+1</button>
+    <button @click="counter--">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      counter:0
+    }
+  },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
